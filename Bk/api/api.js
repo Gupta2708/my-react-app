b33bf7a9241549662938c8c7b995c95c0c04648c
Gupta2708 <guptavaibhav2708@gmail.com>
2025-07-15 17:22:48 +0530

    change in requirement

--- Bk/api/api.js
+++ Bk/api/api.js
@@ -1,13 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: window.location.hostname === 'localhost' 
-    ? 'http://localhost:8102'
-    : window.location.hostname === '127.0.0.1'
-    ? 'http://127.0.0.1:8102'
-    : window.location.hostname.startsWith('192.168.')
-    ? `http://${window.location.hostname}:8102`
-    : `http://${window.location.hostname}:8102`
+  baseURL: "https://my-react-app-weo4.onrender.com"
 });
 
 // Force the correct baseURL for network access
@@ -18,5 +12,3 @@ if (window.location.hostname === '192.168.29.210') {
 // Add this for debugging
 console.log('API baseURL:', api.defaults.baseURL);
 console.log('Current hostname:', window.location.hostname);
-
-export default api;
